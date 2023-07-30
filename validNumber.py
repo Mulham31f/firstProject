8aee39aabb8c2ad8b403095fedb9e3956b0fb579
Mulham31f <139771149+Mulham31f@users.noreply.github.com>
2023-07-30 15:32:10 +0400

    Delete validNumber.py

--- validNumber.py
+++
@@ -1,7 +0,0 @@
-number = input("please enter the number :")
-length = len(number)
-
-if length ==12 and number[0]=="+" and number[1:4]=="968" and number[4:].isdigit():
-    print("valid number")
-else:
-    print("not valid number")
